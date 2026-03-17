9f5e6deedbcd3b1b2450a5a70248c0686b7a2880
Frank Zhang <rgb360@126.com>
2026-03-17 17:23:10 +0800

    Add new packages to diy.sh script

--- N1-IMM/diy.sh
+++ N1-IMM/diy.sh
@@ -12,14 +12,15 @@ function git_sparse_clone() {
 # Default IP
 sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
 
-
 # Add packages
 #添加科学上网源
+git clone --depth 1 https://github.com/ophub/luci-app-amlogic package/amlogic
+git clone --depth 1 https://github.com/sbwml/luci-app-mosdns package/mosdns
+
 git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
-git clone --depth 1 https://github.com/ophub/luci-app-amlogic package/amlogic
+
 #git clone --depth 1 https://github.com/sirpdboy/luci-app-ddns-go package/ddnsgo
-git clone --depth 1 https://github.com/sbwml/luci-app-mosdns package/mosdns
 #git clone --depth 1 https://github.com/sbwml/luci-app-alist package/alist
 #git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall
 
@@ -36,11 +37,11 @@ rm -rf feeds/luci/applications/luci-app-argon-config
 # 替换luci-app-openvpn-server imm源的启动不了服务！
 #rm -rf feeds/luci/applications/luci-app-openvpn-server
 #git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-openvpn-server
+
 # 调整 openvpn-server 到 VPN 菜单
 #sed -i 's/services/vpn/g' package/luci-app-openvpn-server/luasrc/controller/*.lua
 #sed -i 's/services/vpn/g' package/luci-app-openvpn-server/luasrc/model/cbi/openvpn-server/*.lua
 #sed -i 's/services/vpn/g' package/luci-app-openvpn-server/luasrc/view/openvpn/*.htm
-
 
 #替换luci-app-socat为https://github.com/chenmozhijin/luci-app-socat
 #rm -rf feeds/luci/applications/luci-app-socat
