808b92c8d9d3b444062616fd8d3e21e35cfa750c
Frank Zhang <rgb360@126.com>
2026-03-26 10:18:52 +0800

    Clean up diy.sh by removing unused git clone lines
    Removed commented-out git clone commands for various packages.

--- N1-LEDE/diy.sh
+++ N1-LEDE/diy.sh
@@ -18,12 +18,9 @@ git clone -b v5-lua --single-branch --depth 1 https://github.com/sbwml/luci-app-
 
 #git clone -b 18.06 --single-branch --depth 1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 #git clone -b 18.06 --single-branch --depth 1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
-
 #git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/amlogic
 #git clone --depth=1 https://github.com/sirpdboy/luci-app-ddns-go package/ddnsgo
 #git clone --depth=1 https://github.com/sirpdboy/NetSpeedTest package/NetSpeedTest
-
-
 #git clone -b lua --single-branch --depth 1 https://github.com/sbwml/luci-app-alist package/alist
 #git clone --depth=1 https://github.com/gdy666/luci-app-lucky.git package/lucky
 
